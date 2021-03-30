26cbc7f86ca027182c897c07509959d51e82bb39
Simon Mariani <simon.mariani65@gmail.com>
2021-03-30 21:10:40 +0200

    finished the LSTM

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import numpy as np
 import time
 import pickle
+from torch.utils.data import Dataset, DataLoader, RandomSampler
+import torch
 
 def participant(row):
     person = int(row.id.split('.')[1])
@@ -21,7 +23,6 @@ def load_object(filename):
         object_file = pickle.load(input)
 
     return object_file
-
 
 def initial_processing(data):
     data['time'] = pd.to_datetime(data['time'], infer_datetime_format=True)
@@ -52,10 +53,12 @@ def mid_processing(data):
 
     return all_data
 
-
-def final_processing(data, type='mood', k=5, day_value='mean'):
+def final_processing(data, type='mood', k=5, day_value='mean', nmbr_features=4):
     """returns the data which is still sorted per person and per day but every day is now a data point instead
     of a pandas dataframe."""
+
+    #targets = []
+    #final_data = torch.zeros(size=(len(data), len(data[0]), nmbr_features))
 
     for i in range(len(data)):
         prev_vals = [0 for i in range(k)]
@@ -63,11 +66,13 @@ def final_processing(data, type='mood', k=5, day_value='mean'):
         cumulative_average = 0
         exponential_average = 0
 
+        #targets_person = []
         for j in range(len(data[i])):
             all_vals = data[i][j].loc[data[i][j].variable == type]['value']
 
-            # We skip this iteration and treat it as if it never happened when there are no records
+            # If there is some data missing we simply set it to the preious mean
             if len(all_vals) == 0:
+                data[i][j] = torch.Tensor([val, round(moving_average, 1), round(cumulative_average,1), round(exponential_average, 1)])
                 continue
 
             # Decide what value we take from the current day
@@ -79,6 +84,9 @@ def final_processing(data, type='mood', k=5, day_value='mean'):
                 val = all_vals.min()
             else:
                 val = all_vals.mean()
+
+            # First we take out the target
+            #targets_person.append(val)
 
             # Append the new val and calculate the new average
             prev_vals.append(val)
@@ -102,9 +110,46 @@ def final_processing(data, type='mood', k=5, day_value='mean'):
             prev_vals.pop(0)
 
             # Save all the new changes
-            data[i][j] = [val, round(moving_average, 1), round(cumulative_average,1), round(exponential_average, 1)]
+            data[i][j] = torch.Tensor([val, round(moving_average, 1), round(cumulative_average,1), round(exponential_average, 1)])
+
+        #targets.append(targets_person)
+
 
     return data
+
+class MOOD_loader(Dataset):
+
+    def __init__(self, root='processed_data.pkl', train=True):
+
+        data, targets = self.load_data(root)
+
+        self.data = data
+        self.targets = targets
+        self.root = root
+
+    def load_data(self, root):
+        data = load_object(root)
+
+        targets = []
+        for person in data:
+            targets_days = []
+            for i in range(len(person) - 1):  # note that we exclude the last datapoint here
+                targets_days.append(person[i + 1][0])  # the first element is always just the value
+
+            targets.append(targets_days)
+            del person[-1]  # because we cannot predict the mood for the next dat on the last day
+
+        return data, targets
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+
+        sequence = self.data[item]
+        targets = self.targets[item]
+
+        return sequence, targets
 
 def get_data(path='./dataset_mood_smartphone.csv'):
     """Returns the completely processed data given a path"""
@@ -113,12 +158,13 @@ def get_data(path='./dataset_mood_smartphone.csv'):
     data = mid_processing(data)  # this drastically changes the shape of the data
     data = final_processing(data, k=5, type='mood', day_value='mean')  # also changes the shape of the data
 
-
     save_object(data, 'processed_data.pkl')
 
     data = load_object('processed_data.pkl')
 
     print(data[0][0:10])
+    print(len(data))
+    #print(data[1][0][0:10])
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,6 +1,29 @@
 from dataset import load_object
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
+class Basic_LSTM(nn.Module):
 
+    def __init__(self, batch_size, lstm_num_hidden=64, lstm_num_layers=2,
+                 input_dim=4, output_dim=10, device='cuda:0'):
+
+        super(Basic_LSTM, self).__init__()
+
+        self.lstm_num_hidden = lstm_num_hidden
+        self.device = device
+
+        #self.embed = nn.Embedding(vocabulary_size, embed_dim)
+        self.lstm = nn.LSTM(input_dim, lstm_num_hidden, lstm_num_layers, bias=True)
+        self.final = nn.Linear(lstm_num_hidden, output_dim)
+
+    def forward(self, x):
+
+        #x = self.embed(x)
+        x, _ = self.lstm(x)
+        x = self.final(x)
+
+        return x
 
 
 
