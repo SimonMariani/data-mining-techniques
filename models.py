5aa8b470866d98e274e87025d7f0e35118e10a14
pibo98 <pieterbouwman98@gmail.com>
2021-04-13 14:36:56 +0200

    BiLSTM

--- models.py
+++ models.py
@@ -22,6 +22,25 @@ class Basic_LSTM(nn.Module):
 
         return x
 
+class Basic_BiLSTM(nn.Module):
+
+    def __init__(self, lstm_num_hidden=64, lstm_num_layers=2,
+                 input_dim=38, output_dim=1):
+
+        super(Basic_LSTM, self).__init__()
+
+        self.lstm_num_hidden = lstm_num_hidden
+
+        self.lstm = nn.LSTM(input_dim, lstm_num_hidden, lstm_num_layers, bias=True, batch_first=True, bidirectional=True)
+        self.final = nn.Linear(lstm_num_hidden, output_dim)
+
+    def forward(self, x):
+
+        x, _ = self.lstm(x)
+        x = self.final(x)
+
+        return x
+
 class Basic_Net(nn.Module):
 
     def __init__(self, input_dim=38, output_dim=1):
